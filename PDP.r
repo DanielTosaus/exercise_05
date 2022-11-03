496485da8f47d6c3decf088964e5f4234ccad3e2
Daniel Tosaus <249935@vutbr.cz>
2022-11-03 18:46:10 +0100

    PDP Working and correct with lesson example (5,7,8,12,15,20)

--- PDP.r
+++ PDP.r
@@ -3,29 +3,50 @@
 PDP <- function(L){
   
   w <- max(L)
-  print(max)
-  L <- L[! L %in% w]
+  L <- Delete(w, L)
   X <- c(0, w)
-  Place(L, X)
+  Place(L, X, w)
 }
-
-Place <- function(L, X){
+Place <- function(L, X, w){
   
-  if(lengt(L) == 0){
+  
+  if(length(L) == 0){
+    print(sort(X))
     return(X)
   }
   y <- max(L)
-  gen_fragements <- delta(y,X)
-  if(sum(is.element(gen_fragements, X)) == length(gen_fragements)){
+  gen_fragments <- delta(y,X)
+
+  if(sum(is.element(gen_fragments, L)) == length(gen_fragments)){
+    X <- c(X, y)
+    L <-Delete(gen_fragments, L)
+    Place(L, X, w)
+    X <- Delete(y, X)
+    L <- c(L, gen_fragments)
     
   }
   
+  gen_frag <- delta((w-y), X)
+  if(sum(is.element(gen_frag, L))== length(gen_frag)){
+    X <- c(X, (w-y))
+    L <- Delete(gen_frag, L)
+    Place(L,X, w)
+    X <- Delete((w-y), X)
+    L <- c(L, gen_frag)
+    
+  }
+  return()
 }
 
+Delete <- function(w, L){
+  return(L[! L %in% w])
+}
 delta <- function(y, X){
   
   return(abs(X-y))
 }
-L <- c(1, 2)
-PDP(L)
+L <- c(5,7,8,12,15,20)
+
+X <- PDP(L)
+
 
